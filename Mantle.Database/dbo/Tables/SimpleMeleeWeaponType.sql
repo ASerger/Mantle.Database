b53380ec790552985935cddede34e735b3d99843
Andrew <aserger1@gmail.com>
2020-07-06 20:34:21 -0400

    some targetted changes to melee specifically

--- Mantle.Database/dbo/Tables/SimpleMeleeWeaponType.sql
+++ Mantle.Database/dbo/Tables/SimpleMeleeWeaponType.sql
@@ -1,11 +1,10 @@
-CREATE TABLE [dbo].[SimpleWeaponType] (
+CREATE TABLE [dbo].[SimpleMeleeWeaponType] (
     [Id]                INT             IDENTITY (1, 1) NOT NULL,
     [Description]       VARCHAR(20)     NOT NULL, -- club / dagger / greatclub
     [Cost]              NUMERIC(12,2)   NOT NULL,
     [Weight]            VARCHAR(20)     NOT NULL,
-    [DamageType]        INT             NOT NULL, -- lookup
-    [DiceId]            INT             NOT NULL, -- damage lookup
-    [PropertyId]        INT             NOT NULL, -- Reference for various properties
+    [DamageType]        INT             NOT NULL, -- base damage type lookup (slash/bludgeon/etc)
+    [DiceId]            INT             NOT NULL, -- dice throw lookup
     [ModifiedOn]        DATETIMEOFFSET  CONSTRAINT [DF_SimpleWeaponType_ModifiedOn] DEFAULT SYSDATETIMEOFFSET() NOT NULL,
     [ModifiedBy]        VARCHAR(100)    CONSTRAINT [DF_SimpleWeaponType_ModifiedBy] DEFAULT SUSER_SNAME() NOT NULL,
     CONSTRAINT [PK_SimpleWeaponType_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
