55e7363d542542cc868891cc64482842d75a8062
Andrew <aserger1@gmail.com>
2020-07-06 21:20:45 -0400

    fixed file / table name mismatch

--- Mantle.Database/dbo/Tables/BaseWeaponCategory.sql
+++ Mantle.Database/dbo/Tables/BaseWeaponCategory.sql
@@ -3,5 +3,5 @@ CREATE TABLE [dbo].[BaseWeaponCategory] (
     [WeaponType]        VARCHAR(20)     NOT NULL, -- sword / wand / stave
     [ModifiedOn]        DATETIMEOFFSET  CONSTRAINT [DF_BaseWeaponType_ModifiedOn] DEFAULT SYSDATETIMEOFFSET() NOT NULL,
     [ModifiedBy]        VARCHAR(100)    CONSTRAINT [DF_BaseWeaponType_ModifiedBy] DEFAULT SUSER_SNAME() NOT NULL,
-    CONSTRAINT [PK_BaseWeaponType_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_BaseWeaponCategory_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
