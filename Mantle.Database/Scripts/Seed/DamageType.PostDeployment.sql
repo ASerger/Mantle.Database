033e7d7586e089e7bc0959e77385647ce18655f6
Dan <drasmussen49@gmail.com>
2020-07-22 13:10:12 -0400

    Finished postdeploy seed script for damagetype lookup table

--- Mantle.Database/Scripts/Seed/DamageType.PostDeployment.sql
+++ Mantle.Database/Scripts/Seed/DamageType.PostDeployment.sql
@@ -24,3 +24,93 @@ BEGIN
 	SET IDENTITY_INSERT dbo.DamageType OFF
 END
 GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 4)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(4, 'Fire')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 5)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(5, 'Force')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 6)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(6, 'Lightning')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 7)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(7, 'Necrotic')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 8)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(8, 'Piercing')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 9)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(9, 'Poison')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 10)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(10, 'Psychic')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 11)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(11, 'Radiant')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 12)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(12, 'Slashing')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 13)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(13, 'Thunder')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
