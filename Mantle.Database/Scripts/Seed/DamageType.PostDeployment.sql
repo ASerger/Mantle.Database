adf12c1500658ea30a7149ec28ef70dc930002e8
Dan <drasmussen49@gmail.com>
2020-07-22 12:52:16 -0400

    Begun adding seed postdeploy script for dmg type lookup table

---
+++ Mantle.Database/Scripts/Seed/DamageType.PostDeployment.sql
@@ -0,0 +1,26 @@
+﻿IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 1)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(1, 'Acid')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 2)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(2, 'Bludgeoning')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.DamageType WHERE Id = 3)
+BEGIN
+	SET IDENTITY_INSERT dbo.DamageType ON
+	INSERT INTO dbo.DamageType(Id, DamageType)
+	VALUES(3, 'Cold')
+	SET IDENTITY_INSERT dbo.DamageType OFF
+END
+GO
