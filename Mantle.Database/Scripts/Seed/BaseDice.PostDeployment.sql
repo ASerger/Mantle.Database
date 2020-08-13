cbbf9fceec5f295e6ef48a5b6ce7c81af61c66bb
serger <aserger1@gmail.com>
2020-08-13 13:55:41 -0400

    added some reasonable base dice data

--- Mantle.Database/Scripts/Seed/BaseDice.PostDeployment.sql
+++ Mantle.Database/Scripts/Seed/BaseDice.PostDeployment.sql
@@ -6,3 +6,57 @@ BEGIN
 	SET IDENTITY_INSERT dbo.BaseDice OFF
 END
 GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 1)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(1, 1, 4, '1d4')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 2)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(2, 1, 6, '1d6')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 3)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(3, 1, 8, '1d8')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 4)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(4, 1, 12, '1d12')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 5)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(5, 1, 20, '1d20')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 6)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(6, 1, 100, '1d100')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
