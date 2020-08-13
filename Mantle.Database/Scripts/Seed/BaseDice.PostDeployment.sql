48269cff5b29c79504f7af061ed87db5f5830aef
serger <aserger1@gmail.com>
2020-08-13 15:06:07 -0400

    added 1d10

--- Mantle.Database/Scripts/Seed/BaseDice.PostDeployment.sql
+++ Mantle.Database/Scripts/Seed/BaseDice.PostDeployment.sql
@@ -38,7 +38,7 @@ IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 4)
 BEGIN
 	SET IDENTITY_INSERT dbo.BaseDice ON
 	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
-	VALUES(4, 1, 12, '1d12')
+	VALUES(4, 1, 10, '1d10')
 	SET IDENTITY_INSERT dbo.BaseDice OFF
 END
 GO
@@ -47,7 +47,7 @@ IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 5)
 BEGIN
 	SET IDENTITY_INSERT dbo.BaseDice ON
 	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
-	VALUES(5, 1, 20, '1d20')
+	VALUES(5, 1, 12, '1d12')
 	SET IDENTITY_INSERT dbo.BaseDice OFF
 END
 GO
@@ -56,7 +56,16 @@ IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 6)
 BEGIN
 	SET IDENTITY_INSERT dbo.BaseDice ON
 	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
-	VALUES(6, 1, 100, '1d100')
+	VALUES(6, 1, 20, '1d20')
+	SET IDENTITY_INSERT dbo.BaseDice OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.BaseDice WHERE Id = 7)
+BEGIN
+	SET IDENTITY_INSERT dbo.BaseDice ON
+	INSERT INTO dbo.BaseDice(Id, Count, Sides, Description)
+	VALUES(7, 1, 100, '1d100')
 	SET IDENTITY_INSERT dbo.BaseDice OFF
 END
 GO
