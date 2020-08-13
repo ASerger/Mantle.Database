456f0a7f3c28fac76244d5876d5a18a1dc847ef4
serger <aserger1@gmail.com>
2020-08-13 18:03:16 -0400

    added weapon property

--- Mantle.Database/Scripts/Seed/WeaponProperties.PostDeployment.sql
+++ Mantle.Database/Scripts/Seed/WeaponProperties.PostDeployment.sql
@@ -24,3 +24,21 @@ BEGIN
 	SET IDENTITY_INSERT dbo.WeaponProperties OFF
 END
 GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.WeaponProperties WHERE Id = 4)
+BEGIN
+	SET IDENTITY_INSERT dbo.WeaponProperties ON
+	INSERT INTO dbo.WeaponProperties(Id, BasePropertyId, BaseWeaponCategoryId)
+	VALUES(4, 1, 2)
+	SET IDENTITY_INSERT dbo.WeaponProperties OFF
+END
+GO
+
+IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.WeaponProperties WHERE Id = 5)
+BEGIN
+	SET IDENTITY_INSERT dbo.WeaponProperties ON
+	INSERT INTO dbo.WeaponProperties(Id, BasePropertyId, BaseWeaponCategoryId)
+	VALUES(5, 4, 3)
+	SET IDENTITY_INSERT dbo.WeaponProperties OFF
+END
+GO
