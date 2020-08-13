05e2643b1cdf4773f47e2253af0b8404df32ff89
serger <aserger1@gmail.com>
2020-08-13 13:10:02 -0400

    fixed relationship hierarchy for melee proprties and simple weapons

--- Mantle.Database/dbo/Tables/SimpleMeleeWeaponType.sql
+++ Mantle.Database/dbo/Tables/SimpleMeleeWeaponType.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[SimpleMeleeWeaponType] (
     [Cost]                  NUMERIC(12,2)   NOT NULL,
     [Weight]                VARCHAR(20)     NOT NULL,
     [DamageTypeId]          INT             NOT NULL, -- base damage type lookup (slash/bludgeon/etc)
-    [BaseWeaponCategoryId]  INT             NOT NULL, -- club / dagger / greatclub
+    [MeleePropertiesId]     INT             NOT NULL,
     [BaseDiceId]            INT             NOT NULL, -- dice throw lookup
     [ModifiedOn]            DATETIMEOFFSET  CONSTRAINT [DF_SimpleWeaponType_ModifiedOn] DEFAULT SYSDATETIMEOFFSET() NOT NULL,
     [ModifiedBy]            VARCHAR(100)    CONSTRAINT [DF_SimpleWeaponType_ModifiedBy] DEFAULT SUSER_SNAME() NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[SimpleMeleeWeaponType] (
 GO
 
 ALTER TABLE dbo.SimpleMeleeWeaponType
-    ADD CONSTRAINT FK_SimpleMeleeWeaponType_BaseWeaponCategory_Id FOREIGN KEY (BaseWeaponCategoryId) REFERENCES dbo.BaseWeaponCategory(Id);
+    ADD CONSTRAINT FK_SimpleMeleeWeaponType_MeleeProperties_Id FOREIGN KEY (MeleePropertiesId) REFERENCES dbo.MeleeProperties(Id);
 GO
 
 ALTER TABLE dbo.SimpleMeleeWeaponType
