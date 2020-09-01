ecd9908e7d66665325dcbb9ad8d54bae7428260f
serger <aserger1@gmail.com>
2020-09-01 17:42:20 -0400

    removed Id from effect class insert

--- Mantle.Database/Scripts/Seed/EffectClass.PostDeployment.sql
+++ Mantle.Database/Scripts/Seed/EffectClass.PostDeployment.sql
@@ -1,27 +1,27 @@
 ﻿IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.EffectClass WHERE EffectName = 'Minor')
 BEGIN
-	INSERT INTO dbo.EffectClass(Id, EffectName)
+	INSERT INTO dbo.EffectClass(EffectName)
 	VALUES('Minor')
 END
 GO
 
 IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.EffectClass WHERE EffectName = 'Lesser')
 BEGIN
-	INSERT INTO dbo.EffectClass(Id, EffectName)
+	INSERT INTO dbo.EffectClass(EffectName)
 	VALUES('Lesser')
 END
 GO
 
 IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.EffectClass WHERE EffectName = 'Greater')
 BEGIN
-	INSERT INTO dbo.EffectClass(Id, EffectName)
+	INSERT INTO dbo.EffectClass(EffectName)
 	VALUES('Greater')
 END
 GO
 
 IF NOT EXISTS (SELECT TOP 1 Id FROM dbo.EffectClass WHERE EffectName = 'Exceptional')
 BEGIN
-	INSERT INTO dbo.EffectClass(Id, EffectName)
+	INSERT INTO dbo.EffectClass(EffectName)
 	VALUES('Exceptional')
 END
 GO
